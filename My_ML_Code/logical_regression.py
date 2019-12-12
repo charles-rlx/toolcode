7132d195441834ee28447248f9fad540e4b9e527
Unknown <ruanlx22@gmail.com>
2019-12-12 14:05:18 +0100

    add plot_precision_recall_curve

--- My_ML_Code/logical_regression.py
+++ My_ML_Code/logical_regression.py
@@ -1,7 +1,11 @@
-import sklearn.linear_model as sk_linear
+from sklearn import linear_model
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import classification_report
+from sklearn.model_selection import learning_curve
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import plot_precision_recall_curve
+import matplotlib.pyplot as plt
 import csv
 
 
@@ -22,16 +26,47 @@ def get_data(path):
 
     return X, Y
 
+def f1(arr_true, arr_pred):
+	TP, FP, FN, TN = 0, 0, 0, 0
+	for i in range(len(arr_true)):		
+		if arr_pred[i] == 1:
+			if arr_true[i] == 1:
+				TP+=1
+			else:
+				FP+=1 
+		else:
+			if arr_true[i] == 1:
+				FN+=1
+			else:
+				TN+=1
+	Precision = TP / (TP + FP)
+	Recall = TP / (TP + FN)
+	F = Precision * Recall * 2 / (Precision + Recall)
+	return Precision, Recall, F
+
 	#training part:
 X_train, Y_train = get_data(path = '../data/trainingset.csv')
 X_cross, Y_cross = get_data(path = '../data/crossset.csv')
 X_test, Y_test = get_data(path = '../data/testset.csv')
-model = sk_linear.LogisticRegression(penalty='l2',dual=False,C=1.0,n_jobs=1,random_state=20,fit_intercept=True)
+model = linear_model.LogisticRegression(penalty='l2',dual=False,C=1.0,n_jobs=1,random_state=20,fit_intercept=True, solver='newton-cg', max_iter=1000, tol=0.00001)
 model.fit(X_train,Y_train)
+# print(model)
 
 #f1 score in crossset:
 Y_pred = model.predict(X_cross)
 print("crossset f1 score:"+str(f1_score(Y_cross, Y_pred)))
+# print(precision_recall_fscore_support(Y_cross, Y_pred))
+print(f1(Y_cross, Y_pred))
 #f1 score in testset:
 Y_pred_test = model.predict(X_test)
 print("crossset f1 score:"+str(f1_score(Y_test, Y_pred_test)))
+
+
+# from sklearn.svm import SVC
+# clf = SVC(gamma='auto')
+# clf.fit(X_train, Y_train)
+# Y_pred_SVC = clf.predict(X_cross)
+# print("crossset f1 score:"+str(f1_score(Y_cross, Y_pred_SVC)))
+
+fig = plot_precision_recall_curve(estimator=model, X=X_cross, y=Y_cross, sample_weight=None, response_method='auto')
+plt.show(fig)
