98664ed2146037a13f5bc3818153faa14feb0ec9
Unknown <neroztp@gmail.com>
2019-12-16 22:20:17 +0100

    Change the structure of code in starter.py

--- My_ML_Code/starter.py
+++ My_ML_Code/starter.py
@@ -10,21 +10,29 @@ from sklearn import preprocessing
 
 
 if __name__ == '__main__':
-    #DATA
+    #-------GET DATA FROM CSV FILE-------------
     X_train, Y_train = dataprocess.get_data(path = '../data/trainingset.csv')
     X_cross, Y_cross = dataprocess.get_data(path = '../data/crossset.csv')
     X_test, Y_test = dataprocess.get_data(path = '../data/testset.csv')
-    X_train_cross, Y_train_cross = dataprocess.get_data(path='../data/training_cross.csv')
+    X_train_cross, Y_train_cross = dataprocess.get_data(path='../data/train_and_cross.csv')
+    #Check size of data
+    # print(len(Y_train))
+    # print(len(Y_cross))
+    # print(len(Y_test))
+    # print(len(Y_train_cross))
+
+    #-------Preprocess DATA-------------
     #normalize:
     X_train = dataprocess.nor(X_train)
     X_cross = dataprocess.nor(X_cross)
     X_test = dataprocess.nor(X_test)
     X_train_cross = dataprocess.nor(X_train_cross)
-
+    #Add more features for data
     # X_train = dataprocess.addDimensional(X_train)
     # X_cross = dataprocess.addDimensional(X_cross)
     # X_test = dataprocess.addDimensional(X_test)
 
+    #--------Selection Classify Models -----------
     #model selction
     # model_lrr, fig_pr_lrr = models.model_liner_logistic_regression(X_train, Y_train)
     # Y_cross_pred = model_lrr.predict(X_cross)
@@ -33,12 +41,14 @@ if __name__ == '__main__':
     # Y_train_pred = model_svc.predict(X_train)
     # Y_cross_pred = model_svc.predict(X_cross)
     # Y_test_pred = model_svc.predict(X_test)
+    
+    #--------Measurement for Models(Number) ---------
     # # #F1 score:
     # print(measurement.f1(Y_train, Y_train_pred))
     # print(measurement.f1(Y_cross, Y_cross_pred))
     # print(measurement.f1(Y_test, Y_test_pred))
 
-    #draw figue
+    #--------Measurement for Models(Diagram) -----------
     # plt.show(fig_pr_lrr)
     # fig_db_svc = measurement.draw_decision_boundary_2D(X_train, Y_train, X_train, Y_train)
     # plt.show(fig_db_svc)
@@ -46,3 +56,10 @@ if __name__ == '__main__':
     # plt.show(fig_db_svc_2)
     # measurement.plot_valication_curve(X_train_cross, Y_train_cross)
     # measurement.plot_learning_curve(X_train_cross, Y_train_cross)
+
+
+
+
+
+
+
